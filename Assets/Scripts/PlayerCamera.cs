453cfae70a46e0397939dd959281f265a95ff59a
benjavitorgan <vitorganbenjamin@gmail.com>
2022-06-03 14:46:36 -0300

    Avance clase 2 3/6

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -14,6 +14,6 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 3.75f, -2.53f);
+        transform.position = player.transform.position + new Vector3(0, 3f, -7f);
     }
 }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -13,6 +13,15 @@ public class PlayerCollision : MonoBehaviour
     public Text GO;
     public Text nivel;
     float spawnx, spawny, spawnz;
+    public Transform plataforma1;
+    public Transform plataforma2;
+    public Transform plataforma3;
+
+    /*
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    */
 
     // Start is called before the first frame update
     void Start()
@@ -54,6 +63,7 @@ public class PlayerCollision : MonoBehaviour
 
         if (counter > 0)
         {
+
             /*
             if (col.gameObject.name == "InicialBase" || col.gameObject.name == "Plane lvl1")
             {
@@ -65,6 +75,23 @@ public class PlayerCollision : MonoBehaviour
             {
                 nivel.text = "Nivel: 3";
             }*/
+
+            // Nivel 4, el personaje se mueve junto a la base. Creo un if para cada plataforma
+
+            if (col.gameObject.name == "Platforma 1")
+            {
+                gameObject.transform.SetParent (plataforma1);
+            }
+
+            if (col.gameObject.name == "Platforma 2")
+            {
+                gameObject.transform.SetParent (plataforma2);
+            }
+
+            if (col.gameObject.name == "Platforma 3")
+            {
+                gameObject.transform.SetParent (plataforma3);
+            }
 
             if (col.gameObject.name == "Checkpoint")
             {
@@ -85,4 +112,25 @@ public class PlayerCollision : MonoBehaviour
             GO.text = "¡Game Over!";
         }
     }
+
+    /*
+    void OnTriggerStay(Collision col)
+    {
+
+        if (col.gameObject.name == "Platform")
+        {
+            transform.parent = col.transform;
+
+        }
+    }
+
+    void OnTriggerExit (Collision col)
+    {
+        if (col.gameObject.name == "Platform")
+        {
+            transform.parent = null;
+
+        }
+    }
+    */
 }
